8163bda0bf9ec17b47fe0bf2089b84b6e854d17e
JesusMedina21 <jesusmedina0921@gmail.com>
2025-08-25 11:26:39 -0400

    Update settings.py

--- barberstein/settings.py
+++ barberstein/settings.py
@@ -18,7 +18,7 @@ from decouple import config, Csv
 from pathlib import Path
 from datetime import timedelta
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.#
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
